498c8c422986a228e12ab5cc2bbd74d6e6b6870e
Robert Beding <roberadvenbeding@gmail.com>
2022-10-26 14:41:31 +0700

    konversi home page

--- public/script/navbar-scroll.js
+++ public/script/navbar-scroll.js
@@ -1,7 +1,6 @@
-//fungsi jquery untuk nafbar  saat di scroll maka akan muncul navbarnya
 $(function () {
     $(document).scroll(function () {
-      let $nav = $(".navbar-fixed-top");
-      $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height()); // melakukan pengecekan saat di scroll di cek apakah scrolltop lebih panjang dari panjangnya layout dalam hitungan pixel
+      var $nav = $(".navbar-fixed-top");
+      $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
     });
   });
---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,63 @@
+<footer>
+    <div class="conta">
+        <div class="roww">
+              <div class="coll" id="company">
+                  <img src="images/logo.svg" alt="" class="logo">
+                  <p>
+                    Kami Adalah Spesialis sablon dengan bahan terbaik dari kami. kepercayaan kami ada di tangan kalian
+                  </p>
+                  <div class="social">
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-linkedin"></i></a>
+                  </div>
+              </div>
+
+
+              <div class="coll" id="services">
+                 <h3>Services</h3>
+                 <div class="links">
+                    <a href="#">Illustration</a>
+                    <a href="#">Sablon</a>
+                    <a href="#">Poster Design</a>
+                    <a href="#">Card Design</a>
+                 </div>
+              </div>
+
+              <div class="coll" id="useful-links">
+                 <h3>Links</h3>
+                 <div class="links">
+                    <a href="#">About</a>
+                    <a href="#">Services</a>
+                    <a href="#">Our Policy</a>
+                    <a href="#">Help</a>
+                 </div>
+              </div>
+
+              <div class="coll" id="contact">
+                  <h3>Contact</h3>
+                  <div class="contact-details">
+                     <i class="fa fa-location"></i>
+                     <p>Jl Dr Wahidin<br>Kalimantan Barat Indonesia</p>
+                  </div>
+                  <div class="contact-details">
+                     <i class="fa fa-phone"></i>
+                     <p>+6289671702405</p>
+                  </div>
+              </div>
+        </div>
+
+        <div class="container">
+          <div class="row">
+            <div class="col-12 text-center">
+              <p>Copyright @robertbeding 2022</p>
+            </div>
+          </div>
+       </div>
+
+
+
+    </div>
+ </footer>
